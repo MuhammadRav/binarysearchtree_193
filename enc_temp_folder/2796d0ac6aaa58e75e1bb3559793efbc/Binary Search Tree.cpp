ed0c93e06a0c58d62785ab7bd11e40f67d6bff19
MuhammadRav <muhammad.rezza.ft22@mail.umy.ac.id>
2023-07-01 19:22:58 +0700

    p

--- enc_temp_folder/2796d0ac6aaa58e75e1bb3559793efbc/Binary Search Tree.cpp
+++ enc_temp_folder/2796d0ac6aaa58e75e1bb3559793efbc/Binary Search Tree.cpp
@@ -123,48 +123,8 @@ int main()
 		cout << "\nMenu" << endl;
 		cout << "1. Implement insert operation" << endl;
 		cout << "2. perform inorder traversal" << endl;
-		cout << "3. perform preorder traversal" << endl;
-		cout << "4. perform postorder traversal" << endl;
-		cout << "5. Exit" << endl;
-		cout << "\nEnter your choice (1-5) :";
+		cout << "3 perform preorder traversal" << endl;
+		cout << "4. Implement insert operation" << endl;
 
-
-		char ch;
-		cin >> ch;
-		cout << endl;
-		
-		switch (ch)
-		{
-		case'1':
-		{
-			cout << "Enter a word: ";
-			string word;
-			cin >> word;
-			obj.insert(word);
-			break;
-		}
-		case '2':
-		{
-			obj.inorder(obj.ROOT);
-			break;
-		}
-		case'3':
-		{
-			obj.preorder(obj.ROOT);
-			break;
-		}
-		case'4':
-		{
-			obj.postorder(obj.ROOT);
-			break;
-		}
-		case'5':
-			return 0;
-		default:
-		{
-			cout << "Invalid option" << endl;
-			break;
-		}
-		}
 	}
 }
